50630ee6d098d4c9bd9a94717c4ff4d9030d1efd
[Rajan Gautam] <[rajan.softelevation@gmail.com]>
2026-02-13 00:23:21 +0530

    add file

--- test-bash.sh
+++ test-bash.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
 
 book="black hat bash"
 echo "Name of the book is ${book}"
+PUBLISHER="No Starch Press"
+print_name(){
+ local name
+ name="Black Hat Bash"
+ echo "${name} by ${PUBLISHER}"
+}
+print_name
+echo "Variable ${name} will not be printed because it is a local variable."
